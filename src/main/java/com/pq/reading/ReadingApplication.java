7b9cbe4703c3e54affb182c7643f612c969dc6c4
liutao <zhitao0902@163.com>
2019-01-17 22:33:26 +0800

    update

--- src/main/java/com/pq/reading/ReadingApplication.java
+++ src/main/java/com/pq/reading/ReadingApplication.java
@@ -6,15 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-@EnableHystrix
 @MapperScan("com.pq.reading.mapper")
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"com.pq.reading.feign"})
 public class ReadingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
